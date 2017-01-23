67eb54cbac5e4aee822f04a5394b5b09910ef671
lee <leerwilson@gmail.com>
2017-01-23 16:40:50 -0500

    petbuilder app

---
+++ Unit-1/petBuilder.js
@@ -0,0 +1,41 @@
+// Starting State
+// Assume user is registered
+
+// so pet builder page shows
+loggedIn = true;
+
+let name = "";
+// user inputs string
+let ears = "fold";
+// user selects from menu
+let tail = True;
+// user clicks radio button
+let eyes = ["blue", "gray"];
+//etc.
+
+// create a new class to build pets input by users
+class Pet {
+  constructor(name, ears, tail, eyes, furStyle, color, wings, heads) {
+    this.name = name;
+    this.ears = ears;
+    this.tail = tail;
+    this.eyes = eyes;
+    this.furStyle = furStyle;
+    this.color = color;
+    this.wings = wings;
+    this.heads = heads;
+  }
+}
+// except for name, each string would refer to a jpeg of that particular feature
+
+document.querySelector
+// submit button is clicked
+function buildPet = new Pet ("Fluffy", "none", True, ["blue", "gray"], "leopard", "green", True, 3) {
+// add all images to the DOM something like
+// hardcoding the number of elements is not great
+ for (i; i < 7; i++) {
+  document.createElement(div)
+// loop through all elements of a Pet and append images to DOM
+ }
+}
+
