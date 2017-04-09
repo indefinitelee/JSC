db2803be45d423583ae02e6770ed1ebee7fda1ba
lee <leerwilson@gmail.com>
2017-04-09 14:20:40 -0400

    lesson 3 and 4

---
+++ Unit-6/unit-6_lesson-3-code-challenge-kitten-store-part-1_lee-wilson/main.js
@@ -0,0 +1,9 @@
+//jQuery Manipulation
+$(function() {
+    $('.tabs-content').find('button').on('focus', function(e) {
+        var reviewText = $('textarea').val();
+if(reviewText !== "") {
+            $('#panel1 .reviews').find('.media-object:last-child').find('div:last-child').find('p').text(reviewText);
+        }
+    });
+});
